fde63688596194905ddd20f1728343e354d26381
lijingsmallbee <bn0305@qq.com>
2014-06-18 01:46:03 +0800

    修改该文件为带签名的utf8，解决在android下编译器无法识别宽字符串的问题

--- StreetFighter/Classes/Game/LoadingScene.cpp
+++ StreetFighter/Classes/Game/LoadingScene.cpp
@@ -1,4 +1,4 @@
-#include "LoadingScene.h"
+ï»¿#include "LoadingScene.h"
 USING_NS_CC;
 #include "Common/CommonDef.h"
 #include "2d/CCLayer.h"
@@ -7,22 +7,22 @@ bool CLoadingScene::init()
 {
 	auto layercolor = LayerColor::create(Color4B(0,255,0,255));
 	addChild(layercolor);
-	const char* configString = cc_utf16_to_utf8((unsigned short*)(L"Å¬Á¦¼ÓÔØÖÐ"),-1,nullptr,nullptr);
+	const char* configString = cc_utf16_to_utf8((unsigned short*)(L"åŠªåŠ›åŠ è½½ä¸­"),-1,nullptr,nullptr);
 	TTFConfig ttfConfig("fonts/wt021.ttf",32,GlyphCollection::CUSTOM, configString);
 	Size layerSize = layercolor->getContentSize();
-	const char* label1Text = cc_utf16_to_utf8((unsigned short*)(L"Å¬"),-1,nullptr,nullptr);
+	const char* label1Text = cc_utf16_to_utf8((unsigned short*)(L"åŠª"),-1,nullptr,nullptr);
 	auto label1 = Label::createWithTTF(ttfConfig,label1Text, TextHAlignment::CENTER, 100);
 	label1->setPosition(Point(layerSize.width / 2 - 100, layerSize.height /2));
-	const char* label2Text = cc_utf16_to_utf8((unsigned short*)(L"Á¦"),-1,nullptr,nullptr);
+	const char* label2Text = cc_utf16_to_utf8((unsigned short*)(L"åŠ›"),-1,nullptr,nullptr);
 	auto label2 = Label::createWithTTF(ttfConfig,label2Text, TextHAlignment::CENTER, 100);
 	label2->setPosition(Point(layerSize.width / 2 - 50, layerSize.height /2));
-	const char* label3Text = cc_utf16_to_utf8((unsigned short*)(L"¼Ó"),-1,nullptr,nullptr);
+	const char* label3Text = cc_utf16_to_utf8((unsigned short*)(L"åŠ "),-1,nullptr,nullptr);
 	auto label3 = Label::createWithTTF(ttfConfig,label3Text, TextHAlignment::CENTER, 100);
 	label3->setPosition(Point(layerSize.width / 2 , layerSize.height /2));
-	const char* label4Text = cc_utf16_to_utf8((unsigned short*)(L"Ýd"),-1,nullptr,nullptr);
+	const char* label4Text = cc_utf16_to_utf8((unsigned short*)(L"è¼‰"),-1,nullptr,nullptr);
 	auto label4 = Label::createWithTTF(ttfConfig,label4Text, TextHAlignment::CENTER, 100);
 	label4->setPosition(Point(layerSize.width / 2 + 50, layerSize.height /2));
-	const char* label5Text = cc_utf16_to_utf8((unsigned short*)(L"ÖÐ"),-1,nullptr,nullptr);
+	const char* label5Text = cc_utf16_to_utf8((unsigned short*)(L"ä¸­"),-1,nullptr,nullptr);
 	auto label5 = Label::createWithTTF(ttfConfig,label5Text, TextHAlignment::CENTER, 100);
 	label5->setPosition(Point(layerSize.width / 2 + 100, layerSize.height /2));
 	layercolor->addChild(label1);
