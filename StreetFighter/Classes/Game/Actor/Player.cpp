750486ec6c704e4313a141e6c195b6504fe3b026
lijingsmallbee <bn0305@qq.com>
2014-08-04 09:04:27 +0800

    黄帽子资源

--- StreetFighter/Classes/Game/Actor/Player.cpp
+++ StreetFighter/Classes/Game/Actor/Player.cpp
@@ -10,14 +10,14 @@ CPlayer::CPlayer()
 bool CPlayer::init()
 {
 	CActor::init();
-	ArmatureDataManager::getInstance()->addArmatureFileInfo("animData/Player/Player.ExportJson");
+	ArmatureDataManager::getInstance()->addArmatureFileInfo("animData/Player_Cat/Player_Cat.ExportJson");
 	Armature *armature = nullptr;
-	armature = Armature::create("Player");
+	armature = Armature::create("Player_Cat");
 	if(armature)
 	{
 		m_pArmature = armature;
 		m_pArmature->retain();
-		m_pArmature->getAnimation()->playWithIndex(0);
+		m_pArmature->getAnimation()->play("idle");
 		return true;
 	}
 	return false;
